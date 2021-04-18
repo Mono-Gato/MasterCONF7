8f06e38e9add1b8e5e59af7ab18fec7dbdf81cc7
Juan David Niño <monogato.dg@gmail.com>
2021-04-18 10:39:41 -0500

    created function getData

---
+++ src/js/getData.js
@@ -0,0 +1,12 @@
+const API = "https://masterconf.herokuapp.com/"
+
+const getData = async (endpoint) => {
+    const urlAPI = endpoint ? `${API}${endpoint}` : API;
+    try {
+        const response = await fetch(urlAPI)
+        const data = await response.json();
+        return data
+    } catch (error) {
+        console.log("Fetch Error", error)
+    }
+}
